d78511fb3f478092bcbd2a85c977b49f08d822d9
justinformentin <justin.formentin@gmail.com>
2018-11-01 01:12:34 -0400

    Updated theme.js

--- config/theme.js
+++ config/theme.js
@@ -18,8 +18,8 @@ const colors = {
   background: {
     light: '#3e5fbc',
     dark: '#284187',
-  }
-}
+  },
+};
 
 const shadow = {
   card: '0 20px 30px rgba(0, 0, 0, 0.1)',
@@ -40,58 +40,61 @@ const shadow = {
     small: '0 5px 10px rgba(0, 0, 0, 0.25)',
     big: '0 15px 20px rgba(0, 0, 0, 0.13)',
   },
-}
+};
 
-  const gradient = {
-    leftToRight: `linear-gradient(-45deg, ${colors.background.light} 0%, ${colors.background.dark} 100%)`,
-    rightToLeft: `linear-gradient(45deg, ${colors.background.light} 0%, ${colors.background.dark} 100%)`,
-  }
+const gradient = {
+  // eslint-disable-next-line
+  leftToRight: `linear-gradient(-45deg, ${colors.background.light} 0%, ${colors.background.dark} 100%)`,
+  // eslint-disable-next-line
+  rightToLeft: `linear-gradient(45deg, ${colors.background.light} 0%, ${colors.background.dark} 100%)`,
+};
 
-  const transition = {
-    easeInOutCubic: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
-    easeOutBack: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
-    duration: '0.4s',
-  }
+const transition = {
+  easeInOutCubic: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  easeOutBack: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+  duration: '0.4s',
+};
 
-  const theme = {
-    colors,
-    gradient,
-    shadow,
-    breakpoints: {
-      xs: '400px',
-      s: '600px',
-      m: '900px',
-      l: '1200px',
+const theme = {
+  colors,
+  gradient,
+  shadow,
+  breakpoints: {
+    xs: '400px',
+    s: '600px',
+    m: '900px',
+    l: '1200px',
+  },
+  fontFamily: {
+    // eslint-disable-next-line
+    body: `Open Sans,-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
+    // eslint-disable-next-line
+    heading: `Candal, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
+  },
+  layout: {
+    article: '46rem',
+    base: '70rem',
+    big: '83.33333rem',
+  },
+  borderRadius: {
+    default: '0.4rem',
+    round: '100rem',
+  },
+  transitions: {
+    default: {
+      duration: transition.duration,
+      timing: transition.easeInOutCubic,
+      transition: `all ${transition.duration} ${transition.easeInOutCubic}`,
     },
-    fontFamily: {
-      body: `Open Sans,-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
-      heading: `Candal
-      , -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`,
+    boom: {
+      duration: transition.duration,
+      timing: transition.easeOutBack,
+      transition: `all ${transition.duration} ${transition.easeOutBack}`,
     },
-    layout: {
-      article: '46rem',
-      base: '70rem',
-      big: '83.33333rem',
+    headroom: {
+      transition: 'all 0.25s ease-in-out',
     },
-    borderRadius: {
-      default: '0.4rem',
-      round: '100rem',
-    },
-    transitions: {
-      default: {
-        duration: transition.duration,
-        timing: transition.easeInOutCubic,
-        transition: `all ${transition.duration} ${transition.easeInOutCubic}`,
-      },
-      boom: {
-        duration: transition.duration,
-        timing: transition.easeOutBack,
-        transition: `all ${transition.duration} ${transition.easeOutBack}`,
-      },
-      headroom: {
-        transition: 'all 0.25s ease-in-out',
-      },
-    },
-  }
+  },
+};
 
-export default theme
+export default theme;
